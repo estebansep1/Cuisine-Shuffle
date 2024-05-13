3b6954fc5efc77891de07e497828b88ebfcea30d
Esteban Sepulveda <tebansd@icloud.com>
2024-05-13 12:59:26 -0700

    setting up frontend for UI implementation

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from './Components/Home';
+import ChoosePage from './Components/ChoosePage';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+      <Router>
+        <div className='w-screen h-screen'>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/choose' element={<ChoosePage />} />
+          </Routes>
+        </div>
+      </Router>
   );
 }
 
---
+++ client/src/Components/RestaruantCard.jsx
@@ -0,0 +1,9 @@
+const RestaurantCard = ({restaurant, onClick}) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default RestaurantCard;
