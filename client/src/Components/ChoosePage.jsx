fd2dacb0042290b6987ec438f25251c0b4d4e054
Esteban Sepulveda <tebansd@icloud.com>
2024-05-13 14:56:08 -0700

    finished UI and fetching API code... need to fix mobile styling

--- client/src/Components/ChoosePage.jsx
+++ client/src/Components/ChoosePage.jsx
@@ -1,9 +1,128 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import Confetti from "react-confetti";
+import RestaurantCard from "./RestaruantCard";
+import { useNavigate } from "react-router-dom";
 
-function ChoosePage() {
+function Choose() {
+  const [loading, setLoading] = useState(false);
+  const [restaurants, setRestaurants] = useState([]);
+  const zipCodes = [92530, 92595, 92562]; 
+  const navigate = useNavigate();
+  
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+  const fetchRestaurants = async () => {
+    setLoading(true);
+    const categories = "all";
+    const apiCalls = zipCodes.map((location) => {
+      const apiUrl = `${process.env.REACT_APP_API_URL}/api/yelp?location=${encodeURIComponent(location)}&categories=${encodeURIComponent(categories)}`;
+      return fetch(apiUrl)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network error");
+          }
+          return response.json();
+        })
+        .then((data) => data.businesses || []);
+    });
+
+    try {
+      const results = await Promise.all(apiCalls);
+      let allRestaurants = results.flat();
+      shuffleArray(allRestaurants);
+      allRestaurants = allRestaurants.slice(0, 20); 
+      setRestaurants(allRestaurants);
+    } catch (error) {
+      console.error(error);
+    }
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchRestaurants();
+  }, []);
+
+  const removeFromFront = () => {
+    setRestaurants((prevRestaurants) => {
+      const newRestaurants = [...prevRestaurants];
+      newRestaurants.shift();
+      return newRestaurants;
+    });
+  };
+
+  const removeFromBack = () => {
+    setRestaurants((prevRestaurants) => {
+      const newRestaurants = [...prevRestaurants];
+      newRestaurants.pop();
+      return newRestaurants;
+    });
+  };
+
+  const redirect = (url) => {
+    window.open(url, "_blank");
+  };
+
+  if (loading || restaurants.length === 0) {
+    return (
+      <div className="flex flex-col justify-center items-center w-full h-full bg-orange-300 gap-12">
+        Loading...
+      </div>
+    );
+  }
+
   return (
-    <div>ChoosePage</div>
-  )
+    <>
+      <div className="flex flex-col justify-center items-center w-full h-full bg-orange-300 gap-12">
+        {restaurants.length > 1 ? (
+          <div className="flex flex-row justify-center items-center w-full h-1/2 gap-24">
+            <RestaurantCard
+              restaurant={restaurants[0]}
+              onClick={removeFromBack}
+            />
+
+            <h1 className="text-5xl font-black text-white">OR</h1>
+
+            <RestaurantCard
+              restaurant={restaurants[restaurants.length - 1]}
+              onClick={removeFromFront}
+            />
+          </div>
+        ) : (
+          <div className="flex flex-col justify-center items-center w-full h-full gap-12">
+            <Confetti />
+
+            <h1 className="text-8xl font-black text-white animate-bounce">
+              Winner!
+            </h1>
+            <div className="flex flex-row justify-center items-center w-full h-1/2 gap-24">
+              <RestaurantCard
+                restaurant={restaurants[0]}
+                onClick={() => {
+                  redirect(restaurants[0].url);
+                  console.log(restaurants[0].url);
+                }}
+              />
+            </div>
+          </div>
+        )}
+        <button
+          className="mt-12 px-24 py-4 bg-white rounded-full shadow-sm transition-shadow duration-300 hover:shadow-xl"
+          onClick={() => navigate("/")}
+        >
+          <h1 className="text-2xl font-bold text-orange-400 uppercase">
+            Home
+          </h1>
+        </button>
+
+      </div>
+    </>
+  );
 }
 
-export default ChoosePage
+export default Choose;
--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -6,13 +6,13 @@ export default function Home() {
 
   return (
     <>
-      <div className="flex flex-col justify-center items-center w-full h-full bg-orange-400 gap-12">
-        <div className="flex flex-row justify-center items-center w-full gap-12">
+      <div className="flex flex-col justify-center items-center w-full h-screen bg-orange-400">
+        <div className="flex flex-col items-center gap-6">
           <img className="w-48 h-48" src="/assets/chefs.svg" alt="logo" />
           <h1 className="text-8xl font-black text-white uppercase">Cuisine Shuffle</h1>
         </div>
         <button
-          className="px-24 py-4 bg-white rounded-full shadow-sm transition-shadow duration-300 hover:shadow-xl"
+          className="mt-12 px-24 py-4 bg-white rounded-full shadow-sm transition-shadow duration-300 hover:shadow-xl"
           onClick={() => navigate("/choose")}
         >
           <h1 className="text-2xl font-bold text-orange-400 uppercase">
